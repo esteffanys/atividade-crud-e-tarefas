650ec6076ab0ca7aabc3ae74007a52e7d5862d36
João Victor Nunes dos Santos <94072224+JVictor1012@users.noreply.github.com>
2024-01-02 14:46:52 +0000

    .

--- tasks/lib/tasks.ex
+++ tasks/lib/tasks.ex
@@ -1,32 +1,77 @@
 defmodule Tasks do
 
-  def rot(angulo) do # Rotação, usa trigonometria para rotacionar os pontos
+  def principal do
+    #pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+    loop()
+  end
+
+  def loop() do
+
+    #pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+
+    IO.puts "Sistema Final"
+    IO.puts "============="
+    IO.puts "1. Rotacionar"
+    IO.puts "2. Translação"
+    IO.puts "3. Reflexão"
+    IO.puts "4. Escala"
+    IO.puts "5. Sair"
+
+    IO.puts "Entre com sua opção: "
+    opcao = IO.gets("") |> String.trim |> String.to_integer()
+
+    case opcao do
+      1 -> rot()
+      2 -> trans()
+      3 -> ref()
+      4 -> escala()
+      5 -> sair()
+      _ -> 
+        IO.puts "Opção inválida"
+        loop()
+    end
+  end
+
+  def rot() do # Rotação, usa trigonometria para rotacionar os pontos
+
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
 
-    Enum.map(pontos_fixos, fn ponto ->
+    IO.puts "digite o angulo de rotação"
+    angulo = IO.gets("") |> String.trim |> String.to_integer()
+
+    pontos = Enum.map(pontos_fixos, fn ponto ->
       Task.async(fn ->
         {x, y} = ponto
         {x * :math.cos(angulo) - y * :math.sin(angulo),
          x * :math.sin(angulo) + y * :math.cos(angulo)}
       end)
-    end) |> Enum.map(&Task.await/1) 
+    end) |> Enum.map(&Task.await/1)
+    IO.inspect pontos
+    loop() 
+
   end
 
-  def trans(dx, dy) do #  Translação soma os valores a cada ponto 
+  def trans() do #  Translação soma os valores a cada ponto 
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+    IO.puts "Digite os valores para somar"
+    {dx, dy} = IO.gets("") |> String.trim |> String.to_integer()
 
-    Enum.map(pontos_fixos, fn ponto ->
+    pontos = Enum.map(pontos_fixos, fn ponto ->
       Task.async(fn ->
         {x, y} = ponto
         {x + dx, y + dy}
       end)
     end) |> Enum.map(&Task.await/1) 
+    IO.inspect pontos
+    loop()
   end
 
-  def ref(eixo) do # Reflexão, inverte o sinal da coordenada x ou y.
+  def ref() do # Reflexão, inverte o sinal da coordenada x ou y.
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+    IO.puts "digite o eixo x ou y"
+    eixo = IO.gets("") |> String.trim |> String.to_integer()
 
-    Enum.map(pontos_fixos, fn ponto ->
+    resultado = Enum.map(pontos_fixos, fn ponto ->
       Task.async(fn ->
         {x, y} = ponto
         case eixo do
@@ -35,16 +80,31 @@ defmodule Tasks do
         end
       end)
     end) |> Enum.map(&Task.await/1) 
+    IO.inspect resultado
+    loop()
   end
 
-  def escala(sx, sy) do # Escala, aumenta o poligono multiplicando as coordenadas por valores.
+  def escala() do # Escala, aumenta o poligono multiplicando as coordenadas por valores.
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+    IO.puts "digite os valores a multiplicar"
+    {sx, sy} = IO.gets("") |> String.trim |> String.to_integer()
 
-    Enum.map(pontos_fixos, fn ponto ->
+    resultado = Enum.map(pontos_fixos, fn ponto ->
       Task.async(fn ->
         {x, y} = ponto
         {x * sx, y * sy}
       end)
     end) |> Enum.map(&Task.await/1) 
+    IO.inspect resultado
+    loop()
   end
+
+  def sair do
+    IO.puts "Sistema encerrado."
+    :ok
+  end
+
+
 end
+
+Tasks.principal()
