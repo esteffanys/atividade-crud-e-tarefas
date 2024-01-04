4d569b2a2a176f4b47a6c265d716be663b150781
João Victor Nunes dos Santos <94072224+JVictor1012@users.noreply.github.com>
2024-01-04 21:53:41 +0000

    .

--- tasks/lib/tasks.ex
+++ tasks/lib/tasks.ex
@@ -31,23 +31,24 @@ defmodule Tasks do
   def rot() do
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
 
-    IO.puts "Digite o ângulo de rotação:"
-    angulo = IO.gets("") |> String.trim() |> String.to_integer()
+    IO.puts "Digite o angulo para rotação"
+    angulo = IO.gets("") |> String.trim |> String.to_integer()
 
     pontos = Enum.map(pontos_fixos, fn ponto ->
-      Task.await(Task.async(fn ->
+      Task.async(fn ->
         {x, y} = ponto
         {x * :math.cos(angulo) - y * :math.sin(angulo),
-        x * :math.sin(angulo) + y * :math.cos(angulo)}
-      end))
-    end)
+         x * :math.sin(angulo) + y * :math.cos(angulo)}
+      end)
+    end) |> Enum.map(&Task.await/1)
 
-    IO.inspect pontos
-    loop()
+     IO.inspect pontos
+     loop()
+
   end
 
 
-  def trans() do
+  def trans do
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
 
     IO.puts "Digite o valor de x a somar"
@@ -56,45 +57,46 @@ defmodule Tasks do
     IO.puts "Digite o valor de y a somar"
     dy = IO.gets("") |> String.trim |> String.to_integer()
 
-    pontos =
-      Enum.map(pontos_fixos, fn ponto ->
-        Task.async(fn ->
-          {x, y} = ponto
-          {x + dx, y + dy}
-        end)
-      end)
-      |> Enum.map(&Task.await/1)
-
-    IO.inspect pontos
-    loop()
-  end
-
-  def ref() do
-    pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
-
-    IO.puts "digite o eixo x ou y"
-    eixo = IO.gets("") |> String.trim
-
     pontos = Enum.map(pontos_fixos, fn ponto ->
       Task.async(fn ->
         {x, y} = ponto
-        case eixo do
-          "x" -> {x, -y}
-          "y" -> {-x, y}
-        end
+        {x + dx, y + dy}
       end)
     end) |> Enum.map(&Task.await/1)
 
     IO.inspect pontos
     loop()
+
   end
 
-  def escala() do
+  def ref() do
     pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
-    IO.puts "Digite o valor de x a multiplicar"
+
+    IO.puts "Digite o eixo x ou y"
+    eixo = IO.gets("") |> String.trim
+
+    pontos = Enum.map(pontos_fixos, fn ponto->
+      Task.async(fn ->
+      {x, y} = ponto
+      case eixo do
+        "x" -> {x, -y}
+        "y" -> {-x, y}
+      end
+      end)
+    end) |> Enum.map(&Task.await/1)
+
+    IO.inspect pontos
+    loop()
+
+  end
+
+  def escala do
+    pontos_fixos = [{1, 2}, {3, 4}, {5, 6}]
+
+    IO.puts "Digite a coordenada x para multiplicar"
     sx = IO.gets("") |> String.trim |> String.to_integer()
 
-    IO.puts "Digite o valor de y a multiplicar"
+    IO.puts "Digite a coordenada y para multiplicar"
     sy = IO.gets("") |> String.trim |> String.to_integer()
 
     pontos = Enum.map(pontos_fixos, fn ponto ->
@@ -106,6 +108,7 @@ defmodule Tasks do
 
     IO.inspect pontos
     loop()
+
   end
 
   def sair do
